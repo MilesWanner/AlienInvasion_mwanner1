213d949f3c9718f16a8fdd61574637efc2dccd3a
Miles Wanner <mwanner1@student.cscc.edu>
2026-07-21 10:37:49 -0400

    Update ship rotation, placement, and vertical movement.

--- ship.py
+++ ship.py
@@ -7,20 +7,21 @@ class Ship:
         self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
-        self.rect.midbottom = self.screen_rect.midbottom
-        self.moving_right = False
-        self.moving_left = False
+        self.rotated_image = pygame.transform.rotate(self.image, -90)
+        self.rect = self.rotated_image.get_rect()
+        self.rect.midleft = self.screen_rect.midleft
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
-            self.rect.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += self.settings.ship_speed
 
     def blitme(self):
-        self.screen.blit(self.image, self.rect)
+        self.screen.blit(self.rotated_image, self.rect)
 
     def center_ship(self):
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midleft = self.screen_rect.midleft
         self.x = float(self.rect.x)
